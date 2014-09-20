6d1026a3a6c43f7f41615c6643f23b55211ff5db
kousu <nick@kousu.ca>
2014-09-20 01:13:22 -0400

    Comments

--- test.socks5.js
+++ test.socks5.js
@@ -1,23 +1,24 @@
 
+if(!WebSocket) {
+  var WebSocket = require("ws");
+}
 var SOCKS5 = require("./socks5.js")
 
 
-
-process.argv.shift()//this removes "/usr/bin/node" and should be a standard part of the startup; it is for python
-
+process.argv.shift() //this removes "/usr/bin/node" and should be a standard part of the startup; it is for python
 
 var target = process.argv[1]
 if(!target) {
   target = "uwaterloo.ca:80"
 }
 
-// auth needs to be handled by some sort of callback..?
 var prx = SOCKS5("ws://localhost:8081");
-
+//prx = WebSocket // to demonstrate drop-in compatibility,
+                  // uncomment this and pass "ws://localhost:8082" as the target
+                  // while running `websockify 8082 real_target`
 var ws = new prx(target)
 
-// this part onwards is drop-in websockets compatible
-
+// Note that from here onwards is the straight WebSockets API
 ws.onopen = function() {
   console.log("Requesting HTTP from ", target)
   this.send("GET / HTTP/1.1\r\n\r\n")  
@@ -29,7 +30,8 @@ ws.onmessage = function(e) {
 }
 
 ws.onerror = function(e) {
-  console.log("It blew up!")
+  console.log("It blew up! "+"["+e.constructor.name+"]"+" follows:")
+  console.log()
   console.log(e)
 }
 
@@ -38,18 +40,3 @@ setTimeout(function() {
   console.log("Timeout.")
   ws.close();
 }, 15*1000)
-/*
-or proxy via 
-
-var prx = new SOCKS5("ws://proxy-host.com:3535", "tor.kousu.ca:22")
-
-and prx is simply 
-
-// or with our (currently imaginary) SSH client library:
-var ssh = new SSH(prx)
-ssh.onconnect = function(e) {
-  ssh.login("user", "pass")
-}
-ssh.onlogin = function(e*
-
-*/
